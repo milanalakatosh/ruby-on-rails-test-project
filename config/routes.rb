60a4ea229c548022df31e5ed5195ab92b53881cf
Milana Lakatosh <milanammk@gmail.com>
2024-05-07 12:17:00 +0300

    comments on articles fixed:comment model created, route for comments added, comments are shown

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
-
-  resources :articles
+  # The comments as a nested resource within articles
+  resources :articles do
+    resources :comments
+  end
 end
