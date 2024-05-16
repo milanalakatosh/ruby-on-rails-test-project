2ac377d424077dbcfb7a17b94b137433e3fc227a
Milana Lakatosh <milanammk@gmail.com>
2024-05-16 11:59:18 +0300

    CommentsController refactored to simplify the code and remove duplication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,20 +2,24 @@ class CommentsController < ApplicationController
   # only authenticated users are allowed to delete comments
   # http_basic_authenticate_with name: "test", password: "test", only: :destroy
 
+  before_action :set_article, only: [:create, :destroy]
+
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article), status: :see_other
   end
 
   private
+    def set_article
+      @article = Article.find(params[:article_id])
+    end
+
     def comment_params
       params.require(:comment).permit(:commenter, :body, :status)
     end
